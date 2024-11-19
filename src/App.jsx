4ae3cf471f597d7b168a3ff9831ff45b2e046db2
Stan <stanislawpera94@gmail.com>
2024-11-19 22:05:25 +0100

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import "./App.css";
+import Navigation from "./components/Navigation";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div>Siema</div>
+    <Navigation/>
+      {/* <Outlet /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+        <App />
+    {/* <RouterProvider router={router}> */}
+    {/* </RouterProvider> */}
   </StrictMode>
-)
+);
