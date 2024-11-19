8de7bc85c25b59ade524d2b0d909678327e38bef
Stan <stanislawpera94@gmail.com>
2024-11-19 23:04:01 +0100

    feat 2

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,28 @@
-import { Link, Outlet } from 'react-router-dom'
-
+import { NavLink, Outlet } from "react-router-dom";
+import clsx from "clsx";
+const StyledNavLink = ({ name, adress }) => {
+  return (
+    <NavLink
+      className={({ isActive }) =>
+        clsx("p-2 rounded-md border-none", { "bg-black text-white": isActive })
+      }
+      to={adress}
+    >
+      {name}
+    </NavLink>
+  );
+};
 const Navigation = () => {
   return (
-    <div>
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='about'>About</Link>
-        <Link to='contact'>Contact</Link>
+    <div className="flex flex-col items-center gap-y-10">
+      <nav className="flex gap-x-4">
+        <StyledNavLink name="Home" adress="/" />
+        <StyledNavLink name="About" adress="about" />
+        <StyledNavLink name="Contact" adress="contact" />
       </nav>
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
