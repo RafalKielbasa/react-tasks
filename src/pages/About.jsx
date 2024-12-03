56a8735cdb1efd0ab207e788476b0b22733acadd
Stan <stanislawpera94@gmail.com>
2024-12-03 22:56:41 +0100

    feat 03-12

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,26 +1,23 @@
-import { Link, Outlet } from 'react-router-dom';
-import users from '../../userMockData';
+import { Link, Outlet } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 const About = () => {
-	return (
-		<div className='flex flex-col items-center'>
-			<div>About</div>
-			<div>
-				{users.map(({ imie, id }) => {
-					return (
-						<Link
-							className='p-4'
-							key={id}
-							to={`${id}`}
-						>
-							{imie}
-						</Link>
-					);
-				})}
-			</div>
-			<Outlet />
-		</div>
-	);
+  const { data } = useFetch({ url: "http://localhost:3001/usersList" });
+  return (
+    <div className="flex flex-col items-center">
+      <div>About</div>
+      <div>
+        {data?.map(({ name, id }) => {
+          return (
+            <Link className="p-4" key={id} to={`${id}`}>
+              {name}
+            </Link>
+          );
+        })}
+      </div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,21 @@
+import useFetch from "../hooks/useFetch";
+
 const Contact = () => {
-    return <div>Contact</div>;
-  };
-  
-  export default Contact
+  const { contact } = useFetch({ url: "http://localhost:3001/pages" });
+
+  return (
+    <div className="text-center">
+      <h3 className="text-xl font-bold pb-2">{contact?.title}</h3>
+      <p className="mb-2">{contact?.content}</p>
+      <p className="mb-2">
+        <span className="font-bold">Numer telefonu:</span> {contact?.phone}
+      </p>
+      <p>
+        <span className="font-bold">E-mail: </span>
+        {contact?.email}
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
